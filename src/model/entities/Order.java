f51b17fff6ef6904009ce1d0c0c462e51b814bf4
Vinicius B Silva <vinicius.bsilva@hotmail.com>
2020-04-02 18:11:41 -0300

    Class Order developed

--- src/model/entities/Order.java
+++ src/model/entities/Order.java
@@ -18,10 +18,41 @@ public class Order {
 		orderItems.add(orderItem);
 	}
 
+	public List<Product> getProducts () {
+		List<Product> products = new ArrayList<Product>();
+		for (OrderItem item : orderItems) {
+			products.add(item.getProduct());
+		}
+		return products;
+	}
+	
+	public Double calculateTotal() {
+		Double sum = 0.00;
+		for (OrderItem item : orderItems) {
+			sum += item.calculateItemTotal();
+		}
+		return sum;
+	}
+	
 	public String toString () {
 		Formatter formatter = new Formatter();
+			
+		formatter.format("| Item | Product%-33s | Price%-10.2f | Quantity | Item Total%-5.2f | %n", "", "", "", "");
+		int i = 0;
+		for (OrderItem item : orderItems) {
+			i++;
+			formatter.format("| %-4s | %-40s | %-15.2f | %-8d | %-15.2f | %n", 
+					i, 
+					item.getProduct().getName(), 
+					item.getProduct().getPrice(),
+					item.getQuantity(),
+					item.calculateItemTotal());
+		}
 		
-		//formatter.format(format, args);
+		formatter.format("%n");
+		formatter.format("%n");
+		
+		formatter.format("Total: %.2f", calculateTotal());
 		
 		String result = formatter.toString();
 		formatter.close();
